355f12ca92906146df8a36bd99506124cb56565a
Justin Welsh <justinwelsh33@gmail.com>
2023-08-04 11:12:50 -0500

    modal hidden

--- ui/src/app/services/modal.service.ts
+++ ui/src/app/services/modal.service.ts
@@ -7,4 +7,9 @@ export class ModalService {
   visible: boolean = false;
 
   constructor() {}
+  // TODO: Create a function that returns the value of our modals visability.
+  // TODO: Create a function that toggles visability.
+
+  isModalOpen = () => this.visible;
+  toggleModal = () => (this.visible = !this.visible);
 }
