5fda62988a7761f57059f2f634313f4e9633f8c5
Justin Welsh <justinwelsh33@gmail.com>
2023-08-04 10:54:46 -0500

    Injecting services

--- ui/src/app/shared/modal/modal.component.ts
+++ ui/src/app/shared/modal/modal.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
