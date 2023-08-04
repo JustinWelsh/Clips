ca6987a21f980add06f5907c962117c683076274
Justin Welsh <justinwelsh33@gmail.com>
2023-08-03 21:50:43 -0500

    adding user module

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 
@@ -12,7 +14,8 @@ import { NavComponent } from './nav/nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
